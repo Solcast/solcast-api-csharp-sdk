05a6a9f06dcb7f9347bdea1220e75e8a87f105bb
publish-bot-solcast <nikolay@solcast.com.au>
2025-08-05 01:44:59 +0000

    CI: Update generated SDK for dev on 2025-08-05

---
+++ src/Solcast/Models/Responses/GetForecastKimberLosses.cs
@@ -0,0 +1,37 @@
+using Newtonsoft.Json;
+
+namespace Solcast.Models
+{
+    public class GetForecastKimberLosses
+    {
+        [JsonProperty("request_id")]
+        public string RequestId { get; set; }
+
+        [JsonProperty("latitude")]
+        public double? Latitude { get; set; }
+
+        [JsonProperty("longitude")]
+        public double? Longitude { get; set; }
+
+        [JsonProperty("hours")]
+        public int? Hours { get; set; }
+
+        [JsonProperty("cleaning_threshold")]
+        public int? CleaningThreshold { get; set; }
+
+        [JsonProperty("soiling_loss_rate")]
+        public double? SoilingLossRate { get; set; }
+
+        [JsonProperty("grace_period")]
+        public int? GracePeriod { get; set; }
+
+        [JsonProperty("max_soiling")]
+        public double? MaxSoiling { get; set; }
+
+        [JsonProperty("initial_soiling")]
+        public double? InitialSoiling { get; set; }
+
+        [JsonProperty("rain_accum_period")]
+        public int? RainAccumPeriod { get; set; }
+    }
+}
