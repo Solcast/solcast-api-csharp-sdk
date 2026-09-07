2ef173f38a485844bb9f902dd63883dd0af40ed0
publish-bot-solcast <nikolay@solcast.com.au>
2026-09-07 06:23:33 +0000

    CI: Regenerated the SDK for prod on 2026-09-07

--- tests/Solcast.Tests/Generated/LiveTests.cs
+++ tests/Solcast.Tests/Generated/LiveTests.cs
@@ -36,13 +36,12 @@ namespace Solcast.Tests.Generated
         {
             var response = await client.Data.Live.AdvancedPvPower.GetAsync(request =>
             {
-                request.QueryParameters.OutputParameters = ["air_temp", "dni", "ghi"];
                 request.QueryParameters.ResourceId = "ba75-e17a-7374-95ed";
             });
 
             Assert.That(response, Is.Not.Null);
             Assert.That(response!.EstimatedActuals, Is.Not.Null.And.Not.Empty);
-            Assert.That(response.EstimatedActuals![0].Ghi, Is.Not.Null, "the field the call asked for");
+            Assert.That(response.EstimatedActuals![0].PeriodEnd, Is.Not.Null, "the records came back populated");
         }
 
         /// <summary>Get soiling loss estimated actuals using the HSU model for near real-time and past 7 days for the requested location.</summary>
@@ -57,7 +56,7 @@ namespace Solcast.Tests.Generated
 
             Assert.That(response, Is.Not.Null);
             Assert.That(response!.EstimatedActuals, Is.Not.Null.And.Not.Empty);
-            Assert.That(response.EstimatedActuals![0].Ghi, Is.Not.Null, "the field the call asked for");
+            Assert.That(response.EstimatedActuals![0].PeriodEnd, Is.Not.Null, "the records came back populated");
         }
 
         /// <summary>Get soiling loss estimated actuals using the Kimber model for near real-time and past 7 days for the requested location.</summary>
@@ -72,7 +71,7 @@ namespace Solcast.Tests.Generated
 
             Assert.That(response, Is.Not.Null);
             Assert.That(response!.EstimatedActuals, Is.Not.Null.And.Not.Empty);
-            Assert.That(response.EstimatedActuals![0].Ghi, Is.Not.Null, "the field the call asked for");
+            Assert.That(response.EstimatedActuals![0].PeriodEnd, Is.Not.Null, "the records came back populated");
         }
 
         /// <summary>Get irradiance and weather estimated actuals for near real-time and past 7 days for the requested location, derived from satellite (clouds and irradiance over non-polar continental areas) and numerical weather models (other data).</summary>
@@ -81,14 +80,13 @@ namespace Solcast.Tests.Generated
         {
             var response = await client.Data.Live.RadiationAndWeather.GetAsync(request =>
             {
-                request.QueryParameters.OutputParameters = ["air_temp", "dni", "ghi"];
                 request.QueryParameters.Latitude = -33.856784;
                 request.QueryParameters.Longitude = 151.215297;
             });
 
             Assert.That(response, Is.Not.Null);
             Assert.That(response!.EstimatedActuals, Is.Not.Null.And.Not.Empty);
-            Assert.That(response.EstimatedActuals![0].Ghi, Is.Not.Null, "the field the call asked for");
+            Assert.That(response.EstimatedActuals![0].PeriodEnd, Is.Not.Null, "the records came back populated");
         }
     }
 }
